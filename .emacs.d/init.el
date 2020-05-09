c890dd842fb5f1eca21b9934dff53268638a2e42
Pavel Ivanov <sudokey@yandex-team.ru>
2020-05-09 19:26:08 +0300

    Added emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,16 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(load-file "~/.emacs.d/packages.el")
